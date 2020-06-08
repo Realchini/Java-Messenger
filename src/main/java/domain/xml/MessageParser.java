132e5d96e22b0853d69f3216d0afcd9ab5d39d54
Realchini <realchini@users.noreply.github.com>
2020-06-08 17:46:17 +0300

    формирование message

--- src/main/java/domain/xml/MessageParser.java
+++ src/main/java/domain/xml/MessageParser.java
@@ -7,6 +7,9 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -31,7 +34,35 @@ public class MessageParser extends DefaultHandler {
         LOGGER.debug("Start Element");
         LOGGER.trace("<" + qName);
         if ("message".equals(qName)) {
-            message = Message.newMessage().build();
+            Message.Builder builder = Message.newMessage();
+            for (int i=0; i<attributes.getLength(); i++) {
+                String attrName = attributes.getLocalName(i);
+                String attrValue = attributes.getValue(i);
+                LOGGER.trace(attrName + "=" + attrValue);
+                switch (attrName) {
+                    case "sender":
+                        builder.from(attrValue);
+                        break;
+                    case "receiver":
+                        builder.to(attrValue);
+                        break;
+                    case "id":
+                        builder.id(Long.valueOf(attrValue));
+                        break;
+                    case "moment":
+                        Calendar calendar = Calendar.getInstance();
+                        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss dd-MM-yyyy");
+                        try {
+                            calendar.setTime(format.parse(attrValue));
+                        }
+                        catch (ParseException e) {
+                            LOGGER.error(e.getMessage());
+                            e.printStackTrace();
+                        }
+                        builder.moment(calendar);
+                }
+            }
+            message = builder.build();
         }
         LOGGER.trace(">");
     }
