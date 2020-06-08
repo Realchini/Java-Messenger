23ec7d94ce2fdfcaf57d9717250c224d2f8f70f1
Realchini <realchini@users.noreply.github.com>
2020-06-08 16:22:25 +0300

    коммит два

--- src/main/java/domain/Message.java
+++ src/main/java/domain/Message.java
@@ -1,4 +1,12 @@
 package domain;
 
+import java.util.Calendar;
+
 public class Message {
+    private Long id;
+    private String text;
+    private String userNameFrom, userNameTo;
+    private Calendar moment;
+
+    
 }
