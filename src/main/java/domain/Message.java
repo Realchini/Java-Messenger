4fd4cccd17971a7e9f9c333aa536f6677546ae81
Realchini <realchini@users.noreply.github.com>
2020-06-08 16:23:21 +0300

    геттеры, сеттеры

--- src/main/java/domain/Message.java
+++ src/main/java/domain/Message.java
@@ -8,5 +8,43 @@ public class Message {
     private String userNameFrom, userNameTo;
     private Calendar moment;
 
-    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getUserNameFrom() {
+        return userNameFrom;
+    }
+
+    public void setUserNameFrom(String userNameFrom) {
+        this.userNameFrom = userNameFrom;
+    }
+
+    public String getUserNameTo() {
+        return userNameTo;
+    }
+
+    public void setUserNameTo(String userNameTo) {
+        this.userNameTo = userNameTo;
+    }
+
+    public Calendar getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Calendar moment) {
+        this.moment = moment;
+    }
 }
