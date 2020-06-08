d678185ea971c26fe66e1acc879900d23822da51
Realchini <realchini@users.noreply.github.com>
2020-06-08 21:28:06 +0300

    dgdfgdg

--- src/main/java/server/ChatMessengerServer.java
+++ src/main/java/server/ChatMessengerServer.java
@@ -1,19 +1,20 @@
 package server;
 
 import domain.Message;
+import domain.xml.MessageBuilder;
 import domain.xml.MessageParser;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
+import javax.xml.parsers.*;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
@@ -28,7 +29,7 @@ public class ChatMessengerServer {
     private static AtomicInteger id = new AtomicInteger(0);
     private static Map<Long, Message> messagesList = Collections.synchronizedSortedMap(new TreeMap<Long, Message>());
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
         // Load xml files with previous messeges
         loadMessageXMLFile();
 
@@ -59,6 +60,19 @@ public class ChatMessengerServer {
         saveMessagesXMLFile();
         LOGGER.info("Server stopped.");
         serverSocket.close();
+    }
+
+    private static void saveMessagesXMLFile() throws ParserConfigurationException, IOException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.newDocument();
+        String xmlContent = MessageBuilder.buildDocument(document, ,messagesList.values());
+
+        OutputStream stream = new FileOutputStream(new File(XML_FILE_NAME));
+        OutputStreamWriter out = new OutputStreamWriter(stream, StandardCharsets.UTF_8);
+        out.write(xmlContent+"\n");
+        out.flush();
+        out.close();
     }
 
     private static void loadMessageXMLFile() throws ParserConfigurationException, SAXException, IOException {
