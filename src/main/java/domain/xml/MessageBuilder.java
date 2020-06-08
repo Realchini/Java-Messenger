5291d81ea190384136e15cfad058464dae8ca9cb
Realchini <realchini@users.noreply.github.com>
2020-06-08 21:12:09 +0300

    fdghdfhfhf

---
+++ src/main/java/domain/xml/MessageBuilder.java
@@ -0,0 +1,63 @@
+package domain.xml;
+
+import domain.Message;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSOutput;
+import org.w3c.dom.ls.LSSerializer;
+
+import java.io.StringWriter;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+
+public class MessageBuilder {
+    final static Logger LOGGER = LogManager.getLogger(MessageBuilder.class);
+
+    public static String buildDocument(Document document, Collection<Message> messagesList) {
+        LOGGER.debug("Create document start");
+
+        // Create DOM of XML document
+        Element rootElement = document.createElement("messages");
+        document.appendChild(rootElement);
+        LOGGER.trace("Create root element: "+rootElement.toString());
+        for (Message message: messagesList) {
+            Element messageElement = document.createElement("message");
+            rootElement.appendChild(messageElement);
+            if (message.getId()!=null) {
+                messageElement.setAttribute("id", message.getId().toString());
+
+            }
+            messageElement.setAttribute("sender", message.getUserNameFrom());
+            messageElement.setAttribute("receiver", message.getUserNameTo());
+            messageElement.setAttribute("moment", (new SimpleDateFormat("HH:mm:ss dd-MM-yyyy"))
+                    .format(message.getMoment().getTime()));
+            messageElement.appendChild(document.createTextNode(message.getText()));
+            LOGGER.trace("Create message element: "+messageElement.toString());
+        }
+
+        //XML document string format and setting up coding page
+        DOMImplementation impl = document.getImplementation();
+        DOMImplementationLS implLS = (DOMImplementationLS) impl.getFeature("LS", "3.0");
+
+        LSSerializer ser = implLS.createLSSerializer();
+        ser.getDomConfig().setParameter("format-pretty-print", true);
+
+        LSOutput lsOutput = implLS.createLSOutput();
+        lsOutput.setEncoding("UTF-8");
+
+        Writer stringWriter = new StringWriter();
+        lsOutput.setCharacterStream(stringWriter);
+
+        ser.write(document, lsOutput);
+        String result = stringWriter.toString();
+        LOGGER.debug("Create document end");
+        LOGGER.trace(result);
+        return result;
+    }
+}
---
+++ src/main/java/server/ChatMessengerServer.java
@@ -0,0 +1,92 @@
+package server;
+
+import domain.Message;
+import domain.xml.MessageParser;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ChatMessengerServer {
+    final static Logger LOGGER = LogManager.getLogger(ChatMessengerServer.class)
+    private static final int PORT = 7070;
+    private static final int SERVER_TIMEOUT = 500;
+    private static volatile boolean stop = false;
+    private static AtomicInteger id = new AtomicInteger(0);
+    private static Map<Long, Message> messagesList = Collections.synchronizedSortedMap(new TreeMap<Long, Message>());
+
+    public static void main(String[] args) throws IOException {
+        // Load xml files with previous messeges
+        loadMessageXMLFile();
+
+        // Run thread with quit command handler
+        quitCommandThread();
+
+        // Create new Socket Server
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        LOGGER.info("Server started on port: "+PORT);
+
+        // loop of request in sockets with timeout
+        while(!stop) {
+            serverSocket.setSoTimeout(SERVER_TIMEOUT);
+            Socket socket;
+            try {
+                socket = serverSocket.accept();
+                try {
+                    new ServerThread(socket, id, messagesList);
+                } catch(IOException e) {
+                    LOGGER.error("IO error");
+                    socket.close();
+                }
+            }
+            catch(SocketTimeoutException e) { }
+        }
+
+        // Write messeges into xml file
+        saveMessagesXMLFile();
+        LOGGER.info("Server stopped.");
+        serverSocket.close();
+    }
+
+    private static void loadMessageXMLFile() throws ParserConfigurationException, SAXException {
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        SAXParser parser = factory.newSAXParser();
+        List<Message> messages = new ArrayList<>();
+        MessageParser saxp = new MessageParser(id, messages);
+    }
+
+    private static void quitCommandThread() {
+        new Thread() {
+            @Override
+            public void run() {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                while(true) {
+                    String buf;
+                    try{
+                        buf = br.readLine();
+                        if("quit".equals(buf)) {
+                            stop = true;
+                            break;
+                        }
+                        else {
+                           LOGGER.warn("Type 'quit' for server trmination.");
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
+    }
+}
