4d5e5d4f3cf3f35c202b7f9f065562df2b6cd9c7
Realchini <realchini@users.noreply.github.com>
2020-06-10 00:02:21 +0300

    // решает всё

--- src/main/java/client/AbstractView.java
+++ src/main/java/client/AbstractView.java
@@ -16,7 +16,7 @@ public abstract class AbstractView extends JPanel {
 
     public abstract void initialize();
     public abstract void clearFields();
-    public abstract void initModel();
+    //public abstract void initModel(boolean kek);
 
     protected void addLabelField(JPanel panel, String labelText, Component field) {
         JLabel label = new JLabel(labelText);
